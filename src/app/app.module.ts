6dd7f09cd3f14e6e6bf3cc4f1cc445e2113f59ef
guifornagiero <guifornagiero.c@hotmail.com>
2023-10-09 23:45:13 -0300

    fix: create "components" folder and correct paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CountdownComponent } from './countdown/countdown.component';
+import { CountdownComponent } from './components/countdown/countdown.component';
+import { PlayerCardComponent } from './components/player-card/player-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CountdownComponent
+    CountdownComponent,
+    PlayerCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
